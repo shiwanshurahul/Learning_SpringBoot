ac34b7ad5921305af448c3801146efc6b7061cfb
shiwanshurahul <110823952+shiwanshurahul@users.noreply.github.com>
2025-08-05 16:50:15 +0530

    commiting Learning SpringBoot

--- FirstSpringBoot/src/main/java/Shiwanshu/FirstSpringBoot/FirstSpringBootApplication.java
+++ FirstSpringBoot/src/main/java/Shiwanshu/FirstSpringBoot/FirstSpringBootApplication.java
@@ -1,9 +1,10 @@
 package Shiwanshu.FirstSpringBoot;
+//is package ke bahar components ko scan ni krega
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;  //imports the SpringApplication class, which is the entry point for launching Spring Boot apps. It provides the run() method used in the main() method below.
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   //This marks the class as the main Spring Boot application class.
 public class FirstSpringBootApplication {
 
 	public static void main(String[] args) {
@@ -11,3 +12,24 @@ public class FirstSpringBootApplication {
 	}
 
 }
+//@SpringBootApplication -> project me ek hoga jo main class/ entry clas pe lagate
+// incorporates 3 annotation : @Configuration, @EnableAutoConfiguration, @ComponentScan
+// @ComponentScan -> ye spring ko bolta ki scan the beans (@component wale classes)
+// @Configuration -> jis class pe lgate wo config deta, us class me Bean bana skte using @Bean
+
+//src/
+//		└── main/
+//		└── java/
+//		└── Shiwanshu/
+//		└── FirstSpringBoot/         ← main package
+//		├── FirstSpringBootApplication.java  ← @SpringBootApplication
+//               ├── controller/
+//		│   └── MyController.java           ← @RestController
+//                ├── service/
+//		│   └── MyService.java              ← @Service
+//                └── repository/
+//		└── MyRepository.java           ← @Repository
+
+//Everything is inside Shiwanshu.FirstSpringBoot.
+//Spring Boot will automatically detect and manage all components.
+// warna manually configure @SpringBootApplication(scanBasePackages = {"Shiwanshu.FirstSpringBoot", "Shiwanshu.controller"})
--- FirstSpringBoot/src/main/java/Shiwanshu/FirstSpringBoot/myClass.java
+++ FirstSpringBoot/src/main/java/Shiwanshu/FirstSpringBoot/myClass.java
@@ -4,10 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class myClass {
+public class myClass {   //jo file ka name wo public class ka name
 
     @GetMapping("abc")     //localhost:8080/abc pe hi aa rha
 public String sayHi(){
     return "Hi";
+}   //mvn -> builds, manages dependency
+    //pehle jar.original bnta jisme bas code hai, fir fat jar bnta jiisme dependency, packing sb hota and uska size > 3 times of original wala jar
 }
-}
+//Car cr = new Car() = khud se bna rhe object
+//ab spring se bnwa rhe = IOC container se = spring object dega       //IOC container me bahut sare objects hote
+// Application context is a way to implement IOC container
+//kisi class pe @component annotation hai toh us class ko rkh lega IOC
+//spring bean -> object ko bolte jo IOC me hai -> @component wala
